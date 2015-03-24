d267443b38ca810ffa3722880dfc0f3a6b6332ff
Thomas Baguet <baguet@unistra.fr>
2015-03-24 23:08:32 +0100

    Fix tmux script with terminal not set (tput don't like it)

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ tmux-irssi
 See https://wiki.archlinux.org/index.php/irssi#irssi_with_nicklist_in_tmux
 
 Requirements irssi properly configured & http://scripts.irssi.org/scripts/nicklist.pl
+
+To install as a systemd service
+(edit tmuxirssi.service first!!!)
+
+$ sudo cp archlinux/tmuxirssi.service /usr/lib/systemd/system/
+$ sudo systemctl enable tmuxirssi
--- tmux_irssi_nicklist.sh
+++ tmux_irssi_nicklist.sh
@@ -1,32 +1,33 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 #
 #  Based on https://wiki.archlinux.org/index.php/irssi#irssi_with_nicklist_in_tmux
 #  needs http://scripts.irssi.org/scripts/nicklist.pl
 #
 
+# Ugly fix but working for my needs for now !
+export TERM=xterm-256color
 T3=$(pgrep -u $USER -x irssi)
 
 irssi_nickpane() {
     tmux setw main-pane-width $(( $(tput cols) - 21));
     tmux splitw -v "cat ~/.irssi/nicklistfifo";
     tmux selectl main-vertical;
-    tmux selectw -t irc;
+    tmux selectw -t irssi;
     tmux selectp -t 0;
-    tmux send-keys -t 0 "/nicklist fifo" C-m;
 }
 
 irssi_repair() {
-    tmux selectw -t irc
+    tmux selectw -t irssi
     (( $(tmux lsp | wc -l) > 1 )) && tmux killp -a -t 0
     irssi_nickpane
 }
 
 if [ -z "$T3" ]; then
-    tmux new-session -d -s irc;
-    tmux new-window -t irc -n irc irssi;
+    tmux new-session -d -s main;
+    tmux new-window -t main -n irssi irssi;
     irssi_nickpane ;
 fi
-    tmux attach-session -d -t irc;
+    tmux attach-session -d -t main;
     irssi_repair ;
 exit 0
